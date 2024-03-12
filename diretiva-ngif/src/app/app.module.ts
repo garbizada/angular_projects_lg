8d98c01897cc5575735f4eb529b32a4ec47d793d
garbizada <caue.justen@hotmail.com>
2024-03-12 15:02:19 -0300

    mouse over with HostListener and HostBinding

--- diretiva-ngif/src/app/app.module.ts
+++ diretiva-ngif/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { NgIfFirstComponent } from './ng-if-first/ng-if-first.component';
 import { NgForFirstComponent } from './ng-for-first/ng-for-first.component';
 import { NgContentFirstComponent } from './ng-content-first/ng-content-first.component';
+import { HighlightMouseDirective } from './shared/highlight-mouse.directive';
+import { DiretivasCustomizadasComponent } from './diretivas-customizadas/diretivas-customizadas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NgIfFirstComponent,
     NgForFirstComponent,
-    NgContentFirstComponent
+    NgContentFirstComponent,
+    HighlightMouseDirective,
+    DiretivasCustomizadasComponent
   ],
   imports: [
     BrowserModule,
