1941c603511ff792d35655df52591479fd1edc20
garbizada <caue.justen@hotmail.com>
2024-04-22 16:22:49 -0300

    Injecting one service to another

--- diretiva-ngif/src/app/app.module.ts
+++ diretiva-ngif/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { HighlightDirective } from './shared/highlight.directive';
 import { CursosService } from './cursos/cursos.service';
 import { CriarCursoModule } from './criar-curso/criar-curso.module';
 import { CursosModule } from "./cursos/cursos.module";
+import { LogService } from './shared/log.service';
 
 
 @NgModule({
@@ -30,6 +31,7 @@ import { CursosModule } from "./cursos/cursos.module";
     CriarCursoModule,
     CursosModule,
   ],
+  providers: [LogService],
   //providers: [CursosService],
   bootstrap: [AppComponent]
 })
